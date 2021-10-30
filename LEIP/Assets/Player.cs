77258ec678a337a1708fa786b346958b1f615586
Ermes David <ermesdavidg.a@gmail.com>
2021-10-30 00:45:21 -0500

    Coliciones
    hice el scrip para las coliciones y parte de la animacion de la escena del pueblo

--- LEIP/Assets/Player.cs
+++ LEIP/Assets/Player.cs
@@ -6,16 +6,24 @@ public class Player : MonoBehaviour
 {
     public float speed = 4f;
     Animator anim;
+    Rigidbody2D rb2d;
+    Vector2 mov;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Dectectamos el movimiento en un vector
+        mov = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+            );
+        /*
         Vector3 mov = new Vector3(
                 Input.GetAxisRaw("Horizontal"),
                 Input.GetAxisRaw("Vertical"),
@@ -26,8 +34,21 @@ public class Player : MonoBehaviour
             transform.position + mov,
             speed * Time.deltaTime
             );
-
-        anim.SetFloat("MovX", mov.x);
-        anim.SetFloat("MovY", mov.y);
+        */
+        if (mov != Vector2.zero)
+        {
+            anim.SetFloat("MovX", mov.x);
+            anim.SetFloat("MovY", mov.y);
+            anim.SetBool("Walking", true);
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+        }
+    }
+    private void FixedUpdate()
+    {
+        //Nos movemos en el fixed por las físicas
+        rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
     }
 }
