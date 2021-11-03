d5025c41552aa644d71a6a38c0652f98d70a360b
Ermes David <ermesdavidg.a@gmail.com>
2021-11-02 18:26:29 -0600

    Camara, Transiciones y Animaciones
    Ya está el seguimiento de la camara. Teletransportar escenas, y correción de animaciones

---
+++ LEIP/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 4f;
+    Animator anim;
+    Rigidbody2D rb2d;
+    Vector2 mov;
+    public VectorValue startingPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        transform.position = startingPosition.initialValue;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Dectectamos el movimiento en un vector
+        mov = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+            );
+        /*
+        Vector3 mov = new Vector3(
+                Input.GetAxisRaw("Horizontal"),
+                Input.GetAxisRaw("Vertical"),
+                0
+        );
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            transform.position + mov,
+            speed * Time.deltaTime
+            );
+        */
+        if (mov != Vector2.zero)
+        {
+            anim.SetFloat("MovX", mov.x);
+            anim.SetFloat("MovY", mov.y);
+            anim.SetBool("Walking", true);
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+        }
+    }
+    private void FixedUpdate()
+    {
+        //Nos movemos en el fixed por las físicas
+        rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
+    }
+}
