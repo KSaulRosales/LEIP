70d74b9ce8e65111a92ecdefa502a1a2f91c1830
Ermes David <ermesdavidg.a@gmail.com>
2021-11-18 14:13:39 -0600

    Textos a NPC
    Parte 1 sigo con el mismo problema que no sale la segunda y la tercera linea

---
+++ LEIP/Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { FreeRoam, Battle, Dialog }
+
+public class GameController : MonoBehaviour
+{
+    //[SerializeField] PlayerController playerController;
+    // [SerializeField] BattleSystem battleSystem;
+    [SerializeField] Camera worldCamera;
+
+    GameState state;
+    private void Awake()
+    {
+        //conditionsDB.Init();
+    }
+    void Start()
+    {
+        // playerController.OnEncountered += StartBattle;
+        //battleSystem.OnBattleOver += EndBattle;
+
+        DialogManager.Instance.OnShowDialog += () =>
+        { 
+            state = GameState.Dialog;
+
+        };
+
+        DialogManager.Instance.OnCloseDialog += () =>
+        {
+            if (state == GameState.Dialog)
+                state = GameState.FreeRoam;
+        };
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(state == GameState.Dialog)
+        {
+            DialogManager.Instance.HandleUpdate();
+        }
+    }
+}
