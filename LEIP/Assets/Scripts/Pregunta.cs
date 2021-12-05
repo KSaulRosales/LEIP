f4dc630729b16d95708f2580279fd7a32ec4e721
Ermes David <79325338+Deyvider@users.noreply.github.com>
2021-12-04 22:39:07 -0600

    Se creo el canva de la pregunta principal
    Este pregunta hace para hacer referencia  si quiere pasar a la otra zona de la carretera, falta darle funciones a los botones

---
+++ LEIP/Assets/Scripts/Pregunta.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pregunta : MonoBehaviour
+{
+    public GameObject Menupregunta;
+    [SerializeField] GameObject player;
+
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            Menupregunta.SetActive(true);
+            player.SetActive(false);
+
+        }
+    }
+}
